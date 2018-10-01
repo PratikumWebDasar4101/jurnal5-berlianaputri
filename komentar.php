051e7a70ce7f2e713a0dbd2cd54ac2f1616d17e0
berlianaputri <42725251+berlianaputri@users.noreply.github.com>
2018-10-01 10:54:03 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,7 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Komentar</title>
+</head>
+<body>
+	<form action="" method="post">
+		Komentar : <textarea name="komen" rows="5" cols="40"></textarea>
+		<input type="submit" name="submit" value="KIRIM">
+	</form>
+</body>
+</html>
 <?php
-//untuk yg komentar kalian bebas mau menggunakan 2 file atau satu file php
-
-
-
-
+	if (is_null($_POST['komen'])){
+		echo "Komentar masih kosong";
+	}elseif (strlen($_POST['komen'])< 5){
+		echo "Error";
+	}else{
+		echo strlen($_POST['komen']);
+	}
 ?>
